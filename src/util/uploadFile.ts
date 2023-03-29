8af6be5619d0f7b919d49c9463c940135fd4b7d2
Marc <marcplustwo@gmail.com>
2023-03-29 14:39:20 -0300

    replace node-fetch with axios

--- src/util/uploadFile.ts
+++ src/util/uploadFile.ts
@@ -1,21 +1,35 @@
 import { config } from "../config";
+import fetch from "node-fetch";
+import axios from "axios";
 
 const uploadFile = async (telegramURL: string): Promise<string> => {
-  const resp = await fetch(config.imgUploadURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      url: telegramURL,
-    }),
-  });
+  // const resp = await fetch(config.imgUploadURL, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     url: telegramURL,
+  //   }),
+  // });
 
-  if (!resp.ok) {
+  const resp = await axios.post(
+    config.imgUploadURL,
+    {
+      url: telegramURL,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (resp.status != 200) {
     throw new Error("Error uploading file.");
   }
 
-  const data: { filename: string } = await resp.json();
+  const data: { filename: string } = await resp.data;
 
   return `${config.imgUploadURL}/${data.filename}`;
 };
